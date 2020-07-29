a2a82415556a8e340f0602c6b096ac89f942aa89
Engelbert Niehaus <niehausbert@gmail.com>
2020-07-29 17:09:48 +0200

    Create service4jamulus.sh

---
+++ service4jamulus.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Jamulus Service"
+echo "---------------"
+echo "(1) Start Jamulus Server"
+echo "    sudo service4jamulus.sh start" 
+echo "(2) Stop Jamulus Server"
+echo "    sudo service4jamulus.sh stop" 
